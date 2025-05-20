8b3d6398c68e36193dff35210e6db9e3aeaed0d9
VitorR-Soares <vitorratollasoares@gmail.com>
2025-05-20 09:53:36 -0300

    Frontend

---
+++ controlp3dPI/controlp3dPI/src/main/java/com/senac/controlp3dPI/Controlp3dPiApplication.java
@@ -0,0 +1,13 @@
+package com.senac.controlp3dPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controlp3dPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Controlp3dPiApplication.class, args);
+	}
+
+}
---
+++ controlp3dPI/controlp3dPI/src/test/java/com/senac/controlp3dPI/Controlp3dPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.controlp3dPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Controlp3dPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
